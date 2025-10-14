314b5cc50f5bbf52055b25d8794eb0a94b3c7251
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-14 08:18:18 +0200

    ejercicio 25 completo

--- Tema3/ejercicio24/src/ejercicio24/Multiplos3.java
+++ Tema3/ejercicio24/src/ejercicio24/Multiplos3.java
@@ -15,14 +15,15 @@ public class Multiplos3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int numero;
-       int i;
-       int contador = 0;
+        //Declaro variables 
+       int numero; //para el numero que introducira el usuario
+       int i; //para el bucle for 
+       int contador = 0; //contado de multiplos de 3
        Scanner num = new Scanner(System.in);
         do {
-            System.out.println("Por favor introduzca un numero mayor a 0: ");
+            System.out.println("Por favor introduzca un numero mayor a 0: "); 
             numero = num.nextInt(); //usuario introduce numero por consola
-            if (numero <= 0) {
+            if (numero <= 0) { //si es menor o igual a 0 
                 System.out.println("Error: el numero es igual o inferior a 0.");
             }
         } while (numero <=0);
@@ -37,10 +38,10 @@ public class Multiplos3 {
             }
         }
         System.out.println();
-        if (contador == 0) {
+        if (contador == 0) { //pongo esta dondicion para que me imprima cuando no haya multiplos de 3
             System.out.println("No hay multiplos de 3 en ese rango");
         }
-        else{
+        else{  
         System.out.println("El  total de multiplos de 3 entre 1 y " + numero + " son: " + contador);
         }
     }
