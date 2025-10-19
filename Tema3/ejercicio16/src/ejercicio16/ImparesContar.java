638012d9f3e30dc7972667dd944a6d136c8bb9b7
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-19 21:33:09 +0200

    ejercicio 16 comentado y ok

--- Tema3/ejercicio16/src/ejercicio16/ImparesContar.java
+++ Tema3/ejercicio16/src/ejercicio16/ImparesContar.java
@@ -14,16 +14,16 @@ public class ImparesContar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Declaro dos variables. la "i" sera para recorrer el bucle de 20 a 160. "numeroImpares" para contar el numeros impares
+        //Declaro dos variables. la "i" sera para recorrer el bucle de 20 a 160. Y "cantidadImpares" para contar el numeros impares
         int i = 20;
         int cantidadImpares = 0;
         
         
-        System.out.println("Los numeros impares existentes entre el 20 y 160 son: " ); //imprimo por pantalla los numeros que complen co el if
+        System.out.println("Los numeros impares existentes entre el 20 y 160 son: " ); //imprimo por pantalla los numeros que cumplen con el if
         while(i <=160) {
             if( i % 2 !=0){
                 System.out.print(i);
-            if(i < 159 ){     //añado este if para poner el guón . Lo pongo hasta 159 para que no me imprima el "-" despues de 159
+            if(i < 159 ){     //añado este if para evitar el ultimo guion
                 System.out.print(" - ");
             }
                 cantidadImpares ++;  //sumo 1 al contador de numeros impares
@@ -31,7 +31,7 @@ public class ImparesContar {
           i++;   //sumo 1 al contador del bucle desde 20 a 160
         }
         System.out.println(); // siguiente linea
-        System.out.println( "La cantidad de numeros impares impresos ha sido: " + cantidadImpares); //imprimo cantidad de impares
+        System.out.println( "La cantidad de numeros impares impresos han sido: " + cantidadImpares); //imprimo cantidad de impares
         
     }
     
