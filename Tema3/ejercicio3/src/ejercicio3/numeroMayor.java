7e58b6f05820c10917592ae474d17ccbaf67bdb2
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-19 10:09:48 +0200

    ejercicio 3 comentado y ok

--- Tema3/ejercicio3/src/ejercicio3/numeroMayor.java
+++ Tema3/ejercicio3/src/ejercicio3/numeroMayor.java
@@ -9,7 +9,7 @@ package ejercicio3;
  * @author alumno
  */
 
-import java.util.Scanner;
+import java.util.Scanner;  //importo el Scanner para trabajar con el
 public class numeroMayor {
 
     /**
@@ -17,25 +17,26 @@ public class numeroMayor {
      */
     public static void main(String[] args) {
      //Creo el scanner que lea los datos
-        Scanner numero = new Scanner(System.in);
-        int num1;
-        int num2;
-        int num3;
+        Scanner numero = new Scanner(System.in); //creo un escaner para que el usuario introduzca los datos
+        //Declaro las variables que voy a usar
+        float num1;
+        float num2;
+        float num3;
         System.out.println("Por favor, introduzca un Primer numero:  ");
-        num1 =  numero.nextInt();
+        num1 =  numero.nextFloat(); //guardo el numero introducido por el usuario en la variable num1;
         System.out.println("Introduzca el segundo número: ");
-        num2 = numero.nextInt();
+        num2 = numero.nextFloat(); //guardo el numero introducido por el usuario en la variable num2;
         System.out.println("Introduzca el tercer número: ");
-        num3 = numero.nextInt();
-        //creo los ocndicionales
-        if ((num1 > num2)&(num1 > num3)){
+        num3 = numero.nextFloat(); //guardo el numero introducido por el usuario en la variable num3;
+        //creo los condicionales
+        if ((num1 > num2)&(num1 > num3)){ //si num1 es mayor que num2 y num3. num1 es el mayor
             System.out.println("El numero mayor de los introducidos es: " +num1);
             
         }
-        else if ((num2 > num1)&(num2 >num3)){
+        else if ((num2 > num1)&(num2 >num3)){ //lo mismo con num2
             System.out.println("El numero mayor de los introducidos es: " +num2);
         }
-        else if ((num3 > num1)&(num3 > num2)){
+        else if ((num3 > num1)&(num3 > num2)){ //lo mismo con num3
             System.out.println("El numero mayor de los introducidos es: " +num3);
         }
     }
