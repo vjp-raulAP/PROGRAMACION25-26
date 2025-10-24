b812226b6f817d00b9f3e42d2c84e1e1a005fbb2
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2025-10-24 20:15:52 +0200

    ejercico 2 en proceso creando las clases

---
+++ Tema4/Ejercicio2/src/ejercicio2/Operaciones.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author alumno
+ */
+public class Operaciones {
+    
+    public static int suma(){
+        System.out.println("La suma de " + num1 + );
+    }
+    public static int multiplicacion(){
+        
+    }
+    
+    
+}
---
+++ Tema4/Ejercicio2/src/ejercicio2/SumaProducto.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author alumno
+ */
+import java.util.Scanner;
+public class SumaProducto {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        int numero;
+       Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        numero = entrada.nextInt();
+        
+        if(numero > 10){
+            
+        }
+        else{
+            
+        }
+        
+        
+        
+    }
+    
+}
