d820b3d19afd56321c7ff4dd64d26c87cea786d5
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-24 23:18:59 +0200

    Ejercicio2 . Comentarios explicando el código y los métodos.

--- Tema4/Ejercicio2/src/ejercicio2/Operaciones.java
+++ Tema4/Ejercicio2/src/ejercicio2/Operaciones.java
@@ -7,15 +7,29 @@ package ejercicio2;
 /**
  *
  * @author alumno
+ * 
  */
 //creo la clase operaciones dentro del paquete ejercicio2
 public class Operaciones {
+    
+    /**
+     * Metodo que devuelve la suma de 2 numeros
+     * @param num1
+     * @param num2
+     * @return  la suma de num1 y mun2
+     */
     
     public static int suma(int num1, int num2){
        
        int resultado = num1 + num2;
        return resultado;
     }
+    /**
+     * Método que devulve la multiplicación de 2 métodos
+     * @param num1
+     * @param num2
+     * @return Multiplicación de num1 y num2
+     */
     public static int multiplicacion(int num1, int num2){
          int resultado = num1 * num2;
        return resultado;
--- Tema4/Ejercicio2/src/ejercicio2/SumaProducto.java
+++ Tema4/Ejercicio2/src/ejercicio2/SumaProducto.java
@@ -8,7 +8,7 @@ package ejercicio2;
  *
  * @author alumno
  */
-import java.util.Scanner;
+import java.util.Scanner; 
 public class SumaProducto {
 
     /**
@@ -17,25 +17,26 @@ public class SumaProducto {
     
     
     public static void main(String[] args) {
+        //Declaro las variables que voy a usar
         int num1;
         int num2;        
         int resultado;
-        
+        //Creo la entrada de datos del usuario
        Scanner entrada = new Scanner(System.in);
-        System.out.println("Introduce un numero");
+        System.out.println("Por favor , Introduce un numero: ");
         num1 = entrada.nextInt();
         
-        System.out.println("Introduce el segundo numero. ");
+        System.out.println("Ahora, Introduzca un segundo numero: ");
         num2 = entrada.nextInt();
-        
-        if(num1 > 10){
-            resultado = Operaciones.multiplicacion(num1, num2);
-            System.out.println("La operación que se realizó es producto y el resultado es " + resultado);
-        }
-        else{
-            resultado = Operaciones.suma(num1, num2);
-             System.out.println("La operación que se realizó es suma y el resultado es " + resultado);
-        }
+        //Creo la condicion que me hace ejecutar uno metodo u otro. en este caso si es mayos a 10 multiplicación y si es menor a 10 la suma.
+            if(num1 > 10){
+                resultado = Operaciones.multiplicacion(num1, num2);
+                System.out.println("La operación que se realizó es producto y el resultado es " + resultado);
+            }
+            else{
+                resultado = Operaciones.suma(num1, num2);
+                 System.out.println("La operación que se realizó es suma y el resultado es " + resultado);
+            }
         }
         
         
