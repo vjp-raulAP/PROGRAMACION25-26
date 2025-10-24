c90d40aa24e5a07141def7394e93d64fe471ae6f
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2025-10-24 19:57:12 +0200

    ok

---
+++ Tema4/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+import java.util.Scanner;
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * 
+     * 
+     */
+    public static int pedirNum (){
+        int numero;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        numero = entrada.nextInt();
+        return numero;
+    }
+    
+    public static void main(String[] args) {
+         int numero = Ejercicio1.pedirNum();
+         if(numero > 0){
+             System.out.println("El número es positivo. ");
+         }
+         else {
+             System.out.println(" El número es  nesgativo");
+         }
+        
+    }
+    
+}
