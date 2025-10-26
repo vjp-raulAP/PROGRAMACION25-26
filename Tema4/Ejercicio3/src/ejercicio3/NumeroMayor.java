00ba5a7d2e1e3e7c7cd75c6fffb575db52c484d5
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-26 09:23:45 +0100

    Ejercicio3. Creación de las entradas de usuario
    Creo scanner de entrada de datos del usuario.

--- Tema4/Ejercicio3/src/ejercicio3/NumeroMayor.java
+++ Tema4/Ejercicio3/src/ejercicio3/NumeroMayor.java
@@ -2,19 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio3;
+package ejercicio3; //paquete donde contiene la clase principal
 
 /**
  *
  * @author rulaa
  */
+import java.util.Scanner; //PAra que lea la entrada del usuario
+import utilidades.Comparador; //Importamos la clase comparador del paquete utilidades.
 public class NumeroMayor {
 
     /**
+     * 
      * @param args the command line arguments
+     * 
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner numeroUsuario = new Scanner(System.in);
+        //Pido al usuario un numero.
     }
     
 }
--- Tema4/Ejercicio3/src/utilidades/Comparador.java
+++ Tema4/Ejercicio3/src/utilidades/Comparador.java
@@ -9,5 +9,19 @@ package utilidades;
  * @author rulaa
  */
 public class Comparador {
+    //creo el metodo "encontrarMayor"  estatico para que pueda llamarlo desde cualquier clase que recibe 3 parametros (num1,num2,num3)
+    public static int encontrarMayor (int num1,int num2, int num3){
+        //declaro variable donde guardaré el resultado y inicializo suponiendo que "num1" es mayor.
+        int numMayor;
+        numMayor = num1;
+        if(num2 > num1){
+            numMayor = num2;
+        }
+        if(num3 > numMayor){
+            numMayor = num3;
+        }
+        //introduzco return para que me devuelva resultado.
+        return numMayor;
+    }
     
 }
