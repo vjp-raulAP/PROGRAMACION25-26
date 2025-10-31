f2ad3469989390d4228daee5ee8691e6e71d66f3
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-31 14:41:10 +0100

    ejercicio 9 y 10 completado

---
+++ Tema4/ejercicio10/src/ejercicio10/TablaMultiplicar.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author rulaa
+ */
+import java.util.Scanner;
+public class TablaMultiplicar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num = pedirNumero();
+        mostrarTabla(num);
+    }
+    
+    public static void mostrarTabla(int numero){
+        /**
+         * Metodo que me calcula la tabla de multiplicar con un bucle for pasando por parametros un entero. 
+         */
+        System.out.println("La tabla de multiplicar del numero " + numero + " es: ");
+        for (int i = 0; i < 11; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " * " + i + " = " +resultado );
+        }
+    }
+    
+    public static int pedirNumero(){
+        /**
+         * Metodo por el que recojo un numero introducido por el usuario y lo retorno.
+         */
+        int numeroTabla;
+        Scanner entradaUsuario = new Scanner(System.in);
+        System.out.println("Introduzca un numero para hacer la tabla de multiplicar ");
+        numeroTabla = entradaUsuario.nextInt();
+        return numeroTabla;
+    }
+}
