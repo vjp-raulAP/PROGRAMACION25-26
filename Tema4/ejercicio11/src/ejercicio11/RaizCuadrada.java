c7fa4a796d7d35d8ba91db6ae2bb10b4944945be
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-10-31 15:31:26 +0100

    ejercicio 11. creo método para pedir numero con do while y método calculo raiz

---
+++ Tema4/ejercicio11/src/ejercicio11/RaizCuadrada.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio11;
+
+/**
+ *
+ * @author rulaa
+ */
+import java.util.Scanner;
+public class RaizCuadrada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float num = peticionNumero();
+        double raiz = raizCuadrada(num);
+        
+    }
+    //metodo primero pedir numero que sea positivo
+    public static float peticionNumero(){
+        Scanner numeroUsuario = new Scanner(System.in);
+        float numero;
+        
+        do {
+            System.out.println("Introduzca un numero positivo. ");
+            numero = numeroUsuario.nextFloat();
+            if (numero < 0) {
+                System.out.println("Error. El numero es negativo.");
+                
+            }
+        } while (numero < 0);
+        return numero;
+    }
+    
+    //metodo para calculo  de raiz
+    public static double raizCuadrada(double raiz){
+        raiz = Math.sqrt(raiz);
+        return raiz;
+    }
+    //me
+    
+}
