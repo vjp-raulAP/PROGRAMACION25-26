50e50a7bce68115c0a436ed52098a5f2263a6a95
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-11-05 22:46:38 +0100

    Tema4.ejercicio23 prueba de constantes.  completado y documentado.

---
+++ Tema4/ejercicio23constantes/src/ejercicio23constantes/Constantes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio23constantes;
+
+/**
+ * Clase Constantes en la que guarda la constantes velocidadluz, constanteuniversalgravitatoria y constante de plank
+ * @author rulaa
+ */
+public class Constantes {
+    
+          public final static double VELOCIDADLUZ = 299792458;
+          public final static double CONSTANTEUNIVERSALGRAVITACION = 6.67430e-11;
+          public final static double CONSTANTEPLANK = 6.62607015e-34;
+    
+}
---
+++ Tema4/ejercicio23constantes/src/ejercicio23constantes/PruebaConstantes.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio23constantes;
+
+/**
+ *Clase llamada prueba constante que contiene el main donde se imprime el valor de las constantes
+ * @author rulaa
+ */ 
+public class PruebaConstantes {
+  
+    
+    
+    /** 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("El valor de la velocidad de la luz es " +Constantes.VELOCIDADLUZ +" m/s ");
+        System.out.println("El valor de la constante de gravitación universal es " +Constantes.CONSTANTEUNIVERSALGRAVITACION + " Nm2/kg2 ");
+        System.out.println("El valor de la constante de Plank es " +Constantes.CONSTANTEPLANK +" J*s");
+        
+        
+          
+    }
+    
+}
