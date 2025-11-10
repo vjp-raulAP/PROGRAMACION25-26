b3203cccad9f1a2247478e3d592dda6ddf8e338d
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2025-11-10 18:01:23 +0100

    ejercicio de Bicicleta realizado en clase

--- Tema5/tema5/src/tema5/ProgramacionOrientadaAObjetos.java
+++ Tema5/tema5/src/tema5/ProgramacionOrientadaAObjetos.java
@@ -20,27 +20,47 @@ public class ProgramacionOrientadaAObjetos {
        //1.1 Con constructor parametrizado y estableciendo valores con setters
        
        
-       Coche miCoche = new Coche();
-        System.out.println(miCoche.toString());
-        miCoche.setEspejos(5);
-        System.out.println(miCoche.toString());
-       
-        miCoche.acelerar();
-        
-        System.out.println(miCoche.getEspejos());
-        
-        
-       //1.2 Con constructor parametrizado, inicializando los atributos.
-       //con los valores pasados por parámetro.
-       Coche tuCoche = new Coche("Michelin", "Bajo", 3);
-        System.out.println(tuCoche.toString());
-        //lo mismo que la linea  anterior, porque detecta el toString() automaticamente
-        System.out.println(tuCoche);
-        
-        System.out.println(tuCoche.getRuedas());
-        
-        
-               
+//       Coche miCoche = new Coche();
+//        System.out.println(miCoche.toString());
+//        miCoche.setEspejos(5);
+//        System.out.println(miCoche.toString());
+//       
+//        miCoche.acelerar();
+//        
+//        System.out.println(miCoche.getEspejos());
+//        
+//        
+//       //1.2 Con constructor parametrizado, inicializando los atributos.
+//       //con los valores pasados por parámetro.
+//       Coche tuCoche = new Coche("Michelin", "Bajo", 3);
+//        System.out.println(tuCoche.toString());
+//        //lo mismo que la linea  anterior, porque detecta el toString() automaticamente
+//        System.out.println(tuCoche);
+//        
+//        System.out.println(tuCoche.getRuedas());
+//    Bicicleta bicicleta = new Bicicleta();
+//        System.out.println(bicicleta.ruedas);
+//        System.out.println(bicicleta.getEstaPedaleando());
+//        
+//        
+//        Bicicleta bicicleta2= new Bicicleta();
+//        System.out.println("¿Cuantas bicis hemos creado?");
+//        System.out.println(bicicleta.getNumBicicletas());
+//        
+         // Crear una bicicleta
+        Bicicleta bici = new Bicicleta();
+        System.out.println("¿Está pedaleando al principio? " + bici.getEstaPedaleando());
+
+        // Llamar al método para empezar a pedalear
+        empezarAPedalear(bici);
+
+        System.out.println("¿Está pedaleando después? " + bici.getEstaPedaleando());
     }
-    
+
+    // Método estático, fuera del main
+      public static void empezarAPedalear(Bicicleta bici) {
+        bici.setEstaPedaleando(true);
+        System.out.println("¡La bicicleta ha comenzado a pedalear!");
+        }
+      
 }
