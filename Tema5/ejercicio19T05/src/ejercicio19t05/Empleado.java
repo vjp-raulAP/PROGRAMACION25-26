9dd0619a5c96acec2ac914127890e8499f935eca
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-11-21 00:33:47 +0100

    ejercicio19T05 completado y comentado .

---
+++ Tema5/ejercicio19T05/src/ejercicio19t05/Empleado.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio19t05;
+
+/**
+ *
+ * @author rulaa
+ */
+public class Empleado {
+    //1. Atributos
+    private String nombre;
+    private int horas;
+    private float tarifa;
+    
+    //2.contructor por defecto
+     public Empleado(){
+         this.nombre = "";
+         this.horas = 0;
+         this.tarifa = 0.0f;
+     }
+     
+     //2.1 constructor parametrizado
+     public Empleado (String nombre, int horas, float tarifa){
+         this.nombre = nombre;
+         this.horas = horas;
+         this.tarifa = tarifa;
+     }
+     
+     //getter y setters
+     
+     public String getNombre(){
+         return this.nombre;
+     }
+     public void setNombre(String nombre){
+         this.nombre = nombre;
+     }
+     
+     public int getHoras(){
+         return this.horas;
+     }
+     public void setHoras(int horas){
+         this.horas = horas;
+     }
+     
+     public float getTarifa(){
+         return this.tarifa;
+         
+     }
+     public void setTarifa(float tarifa){
+         this.tarifa = tarifa;
+     }
+     
+     //metodos
+     //metodo para el calculo del salario  (tarifa normal = 40h y  tarifaextra(>40h) = tarifanormal + (tarifanormal/2)
+      //lo creo en la clase test
+     
+     
+     
+     //toString
+     @Override
+     public String toString(){
+         return (this.nombre + " trajabó "+this.horas+ " horas, cobra "+this.tarifa+" euros la hora por lo que le corrsponde un sueldo de "+ test.CalculoTarifaTotal(this.tarifa,this.horas)+"euros");
+    }
+}
---
+++ Tema5/ejercicio19T05/src/ejercicio19t05/test.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio19t05;
+
+/**
+ *
+ * @author rulaa
+ */
+import java.util.Scanner;
+public class test {
+
+    //metodo introducir usuario
+    public static  String pedirNombre(){
+        String nombreEmp;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce el nombre del empleado: ");
+        nombreEmp = entrada.nextLine();
+        return nombreEmp;
+    }
+    
+    public static int pedirNumeroHoras(){
+        int numHorasEmp;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduzca el numero de horas trabajadas: ");
+        numHorasEmp = entrada.nextInt();
+        return numHorasEmp;
+    }
+    
+    public static float pedirTarifEmp(){
+        float tarifaEmp;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduzca la tarifa que se paga al empleado: ");
+        tarifaEmp = entrada.nextFloat();
+        return tarifaEmp;
+        
+    }
+    
+    //metodo para el calculo de las horas.
+    
+    public static float CalculoTarifaTotal(float tarifaEmp,int numHorasEmp){
+        
+        float tarifaHorasExtra = tarifaEmp * 1.5f;  //Calculo de la tarifa extra del empleado.
+        float SalarioTotalEmp;
+        int horasExtra;
+        
+        if(numHorasEmp > 40){
+            horasExtra = numHorasEmp - 40;
+            SalarioTotalEmp = (tarifaEmp * 40) + (horasExtra*tarifaHorasExtra);
+            
+        }else{
+            SalarioTotalEmp = tarifaEmp * numHorasEmp; 
+        }
+        return SalarioTotalEmp;
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        //Crear 3 objetos empleado
+        Empleado empleado1 = new Empleado(pedirNombre(),pedirNumeroHoras(),pedirTarifEmp());
+        Empleado empleado2 = new Empleado(pedirNombre(),pedirNumeroHoras(),pedirTarifEmp());
+        Empleado empleado3 = new Empleado(pedirNombre(),pedirNumeroHoras(),pedirTarifEmp());
+        
+        
+        //implimir el resultado de los 3
+        System.out.println(empleado1);
+        System.out.println(empleado2);
+        System.out.println(empleado3);
+    }
+    
+}
