4e82fd22bb6f02a54e3822f89b8001f6558c04ff
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2025-11-23 18:30:08 +0100

    Ejercicio23T05 creando plantillas Vehiculo y taxi

---
+++ Tema5/ejercicio17T05/src/ejercicio17t05/Ejercicio17T05.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio17t05;
+
+/**
+ *
+ * @author rulaa
+ */
+public class Ejercicio17T05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
