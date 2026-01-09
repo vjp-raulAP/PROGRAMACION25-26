f9aefe75f5f65ed30decee420987819febc96c49
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-01-09 20:12:50 +0100

    ok comienzo ejercicio1 tema 7

---
+++ Tema7/tema07arraystipossimples/src/tema07arraystipossimples/Tema07ArraysTiposSimples.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema07arraystipossimples;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author alumno
+ */
+public class Tema07ArraysTiposSimples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //1. DECLARAR E INICIALIZAR UN ARRAY
+        float[]notas = new float [6];
+        String[] diasSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
+        
+        System.out.println(Arrays.toString(notas));
+        System.out.println(Arrays.toString(diasSemana));
+        System.out.println("Hola mundo");
+    }
+    
+}
