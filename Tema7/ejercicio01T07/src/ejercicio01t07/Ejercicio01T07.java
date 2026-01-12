cfda06ab0a6b92f27a2c7b22b5d207a19dd014c1
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-01-12 16:20:31 +0100

    Update Ejercicio01T07.java

--- Tema7/ejercicio01T07/src/ejercicio01t07/Ejercicio01T07.java
+++ Tema7/ejercicio01T07/src/ejercicio01t07/Ejercicio01T07.java
@@ -14,7 +14,7 @@ public class Ejercicio01T07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+                 
     }
     
 }
