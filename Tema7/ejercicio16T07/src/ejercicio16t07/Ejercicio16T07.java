f5fd9100d1a0d567f98200ed141c68f652403b4d
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2026-01-22 19:38:28 +0100

    ejercicio16T07 con metodos.

--- Tema7/ejercicio16T07/src/ejercicio16t07/Ejercicio16T07.java
+++ Tema7/ejercicio16T07/src/ejercicio16t07/Ejercicio16T07.java
@@ -11,124 +11,142 @@ import java.util.Scanner;
  * @author rulaa
  */
 public class Ejercicio16T07 {
+    
+      static Scanner ENTRADA = new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
+    static String[] ALUMNOS = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+    static String[] ASIGNATURAS = {"Lengua", "Mates", "Historia", "Física"};
+    static float[][] NOTAS = new float[6][4];
+
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        
-        String[] alumnos = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
-        String[] asignaturas = {"Lengua", "Mates", "Historia", "Física"};
-        //creo un array bidimensional 
-        float [] [] notas = new float [6] [4];
-        
-        //bucle do-While para las opciones
-        
+
         int opcion;
-          int i , j;
-        do {            
-            System.out.println("OPCIONES ");
-            System.out.println("1. Rellenar notas");
-            System.out.println("2. Mostrar notas");
-            System.out.println("3. Mejor alumno");
-            System.out.println("4. Alumno con más suspensos");
-            System.out.println("5. Asignatura más difícil");
-            System.out.println("6. Salir");
-            System.out.print("Elige opción: ");
-            
-            opcion  = entrada.nextInt();
-            
+
+        do {
+            mostrarMenu();
+            opcion = ENTRADA.nextInt();
+
             switch (opcion) {
-                case 1:  //Opcion para relleno de notas  con un for anidado en otro. 
-                     for (i = 0; i < alumnos.length; i++) {
-                        System.out.println("Alumno: " + alumnos[i]);
-                        for ( j = 0; j < asignaturas.length; j++) {
-                            System.out.print("Nota de " + asignaturas[j] + ": ");
-                            notas[i][j] = entrada.nextFloat();
-                        }
-                    }
+                case 1:
+                    rellenarNotas();
                     break;
                 case 2:
-                    for ( i = 0; i < alumnos.length; i++) {
-                        System.out.print( alumnos[i] + ": ");
-                        for ( j = 0; j < asignaturas.length; j++) {
-                            System.out.print( notas[i][j] + "  ");
-                        }
-                        System.out.println();
-                    }
-                    
+                    mostrarNotas();
                     break;
                 case 3:
-                    float mejorMedia = 0;
-                    int mejorAlumno = 0;
-                    float mediaNotas;
-                  
-                  
-
-                    for ( i = 0; i < alumnos.length; i++) {
-                        float sumaNotas = 0;
-                          for ( j = 0; j < asignaturas.length; j++) {
-                           sumaNotas+= notas[i][j];
-                        }
-                        mediaNotas = sumaNotas / asignaturas.length;
-
-                        if (mediaNotas> mejorMedia) {
-                            mejorMedia = mediaNotas;
-                            mejorAlumno = i;
-                        }
-                    }
-
-                    System.out.println("Mejor alumno: " + alumnos[mejorAlumno]);
+                    mejorAlumno();
                     break;
                 case 4:
-                    int maxSuspensos = 0;
-                    int alumnoSuspensos = 0;
-
-                    for ( i = 0; i < alumnos.length; i++) {
-                        int suspensos = 0;
-                        for (j = 0; j < asignaturas.length; j++) {
-                            if (notas[i][j] < 5) {
-                                suspensos++;
-                            }
-                        }
-                        if (suspensos > maxSuspensos) {
-                            maxSuspensos = suspensos;
-                            alumnoSuspensos = i;
-                        }
-                    }
-                     System.out.println("Alumno con más suspensos: " + alumnos[alumnoSuspensos]);
+                    alumnoConMasSuspensos();
                     break;
                 case 5:
-                     float peorMedia = 10;
-                     int asignaturaPeor = 0;
-                       float sumaNotas = 0;
-                 
-                
-                    for ( j = 0; j < asignaturas.length; j++) {
-                        
-                        for ( i = 0; i < alumnos.length; i++) {
-                            sumaNotas += notas[i][j];
-                        }
-                      mediaNotas = sumaNotas / alumnos.length;
-
-                        if (mediaNotas < peorMedia) {
-                            peorMedia = mediaNotas;
-                            asignaturaPeor = j;
-                        }
-                    }
-
-                    System.out.println("Asignatura más difícil: " + asignaturas[asignaturaPeor]);
+                    asignaturaMasDificil();
                     break;
                 case 6:
-                    System.out.println("Gracias. un saludo");
+                    System.out.println("Gracias. Un saludo");
                     break;
                 default:
-                    System.out.println("Opcion incorrecta. Seleccion un opción del 1 al 6: ");
+                    System.out.println("Opción incorrecta");
             }
 
         } while (opcion != 6);
-                
     }
-    
+
+    // ---------- MÉTODOS ----------
+
+    static void mostrarMenu() {
+        System.out.println("\nOPCIONES");
+        System.out.println("1. Rellenar notas");
+        System.out.println("2. Mostrar notas");
+        System.out.println("3. Mejor alumno");
+        System.out.println("4. Alumno con más suspensos");
+        System.out.println("5. Asignatura más difícil");
+        System.out.println("6. Salir");
+        System.out.print("Elige opción: ");
+    }
+
+    static void rellenarNotas() {
+        for (int i = 0; i < ALUMNOS.length; i++) {
+            System.out.println("Alumno: " + ALUMNOS[i]);
+            for (int j = 0; j < ASIGNATURAS.length; j++) {
+                System.out.print("Nota de " + ASIGNATURAS[j] + ": ");
+                NOTAS[i][j] = ENTRADA.nextFloat();
+            }
+        }
+    }
+
+    static void mostrarNotas() {
+        //asignaturas
+        for (int i = 0; i < ASIGNATURAS.length; i++) {
+            System.out.print(ASIGNATURAS[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < ALUMNOS.length; i++) {
+            System.out.print(ALUMNOS[i] + ": ");
+            for (int j = 0; j < ASIGNATURAS.length; j++) {
+                System.out.print(NOTAS[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void mejorAlumno() {
+        float mejorMedia = 0;
+        int mejorAlumno = 0;
+
+        for (int i = 0; i < ALUMNOS.length; i++) {
+            float suma = 0;
+            for (int j = 0; j < ASIGNATURAS.length; j++) {
+                suma += NOTAS[i][j];
+            }
+            float media = suma / ASIGNATURAS.length;
+
+            if (media > mejorMedia) {
+                mejorMedia = media;
+                mejorAlumno = i;
+            }
+        }
+
+        System.out.println("Mejor alumno: " + ALUMNOS[mejorAlumno]);
+    }
+
+    static void alumnoConMasSuspensos() {
+        int maxSuspensos = 0;
+        int alumnoSuspensos = 0;
+
+        for (int i = 0; i < ALUMNOS.length; i++) {
+            int suspensos = 0;
+            for (int j = 0; j < ASIGNATURAS.length; j++) {
+                if (NOTAS[i][j] < 5) {
+                    suspensos++;
+                }
+            }
+            if (suspensos > maxSuspensos) {
+                maxSuspensos = suspensos;
+                alumnoSuspensos = i;
+            }
+        }
+
+        System.out.println("Alumno con más suspensos: " + ALUMNOS[alumnoSuspensos]);
+    }
+
+    static void asignaturaMasDificil() {
+        float peorMedia = 10;
+        int asignaturaPeor = 0;
+
+        for (int j = 0; j < ASIGNATURAS.length; j++) {
+            float suma = 0;
+            for (int i = 0; i < ALUMNOS.length; i++) {
+                suma += NOTAS[i][j];
+            }
+            float media = suma / ALUMNOS.length;
+
+            if (media < peorMedia) {
+                peorMedia = media;
+                asignaturaPeor = j;
+            }
+        }
+
+        System.out.println("Asignatura más difícil: " + ASIGNATURAS[asignaturaPeor]);
+    }
 }
