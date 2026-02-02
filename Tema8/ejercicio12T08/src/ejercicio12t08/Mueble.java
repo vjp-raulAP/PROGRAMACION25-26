1e52010b26d7ef70cd940dbe1fc4b8467b731067
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-02-02 18:00:20 +0100

    ejercicio12T08   completado

---
+++ Tema8/ejercicio12T08/src/ejercicio12t08/Mueble.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio12t08;
+
+/**
+ *
+ * @author alumno
+ */
+public class Mueble {
+    
+    //Atributos
+     String nombre;
+    double precio;
+    
+    //constructor
+    
+    public Mueble(){
+        this.nombre = "";
+        this.precio = 0;
+    }
+
+    //constructor parametrizado
+    public Mueble(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    
+    //getter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    
+    //setter
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Mueble{" + "nombre=" + nombre + ", precio=" + precio + '}';
+    }
+    
+} 
+    
+    
+
+   
