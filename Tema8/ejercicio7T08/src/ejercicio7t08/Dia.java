f9d2480a385a13cf1d7582eee78192cf8ec38012
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-02-02 16:29:19 +0100

    ok

--- Tema8/ejercicio7T08/src/ejercicio7t08/Dia.java
+++ Tema8/ejercicio7T08/src/ejercicio7t08/Dia.java
@@ -9,5 +9,56 @@ package ejercicio7t08;
  * @author alumno
  */
 public class Dia {
+    //atributos
+    private String nombreDia;
+    private float temperatura;
+    
+    //constructor parametrizado
+
+    public Dia(String nombreDia, int temperatura) {
+        this.nombreDia = nombreDia;
+        this.temperatura = temperatura;
+    }
+    
+    //constructor por defecto
+    
+    public Dia(){
+        this.nombreDia = "";
+        this.temperatura = 0.0f;
+    }
+
+    
+    //getter y setter
+    public String getNombreDia() {
+        return nombreDia;
+    }
+
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public void setNombreDia(String nombreDia) {
+        this.nombreDia = nombreDia;
+    }
+
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    
+    //metodos
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        return "Dia{" + "nombreDia=" + nombreDia + ", temperatura=" + temperatura + '}';
+    }
+    
+    
     
 }
