61a75541742e281b5010124c2a461269bdaf501d
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2026-02-14 16:39:08 +0100

    ejercicio03T09 completado

--- Tema9/ejercicio03T09/src/ejercicio03t09/Ejercicio03T09.java
+++ Tema9/ejercicio03T09/src/ejercicio03t09/Ejercicio03T09.java
@@ -4,6 +4,8 @@
  */
 package ejercicio03t09;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rulaa
@@ -14,7 +16,19 @@ public class Ejercicio03T09 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String cadena;
+        char []caracteres;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Introduce una cadena de caracteres");
+        cadena =  entrada.nextLine();
+        
+        System.out.println("La cadena es:");
+        for (int i = 0; i < cadena.length(); i++) {
+            caracteres = cadena.toCharArray();
+            
+            System.out.println(caracteres[i]);
+        }
     }
     
 }
