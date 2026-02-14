e696ad1fb2156af90d88650c1bad52355e5eaba0
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2026-02-14 17:08:41 +0100

    ejercicio05T09 completado

--- Tema9/ejercicio05T09/src/ejercicio05t09/Ejercicio05T09.java
+++ Tema9/ejercicio05T09/src/ejercicio05t09/Ejercicio05T09.java
@@ -4,17 +4,66 @@
  */
 package ejercicio05t09;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rulaa
+ * 
+ * 
  */
 public class Ejercicio05T09 {
+    public static Scanner entrada = new Scanner (System.in);  //objeto entrada global
+           //  Método para pedir el día
+    public static String pedirDia() {
+
+        System.out.println("Introduce un día de la semana:");
+        return entrada.nextLine();
+    }
+    
+     //  Método para buscar el día
+    public static int buscarDia(String[] dias, String diaUsuario) {
+
+        int i = 0;
+        int posicion = -1;
+
+        while (i < dias.length) {
+
+            if (diaUsuario.equalsIgnoreCase(dias[i])) {
+                posicion = i;
+            }
+
+            i++;
+        }
+
+        return posicion;
+    }
+      //  Método para mostrar resultado
+    public static void mostrarResultado(int posicion) {
+
+        if (posicion != -1) {
+            System.out.println("Es el " + (posicion + 1) + " día de la semana.");
+        } else {
+            System.out.println("Día no válido.");
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String [] diasSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+       String usuarioDia =pedirDia();
+       
+        
+       
+        int posicion = buscarDia(diasSemana,usuarioDia);
+        
+        
+        mostrarResultado(posicion);
+   
+
     }
     
 }
