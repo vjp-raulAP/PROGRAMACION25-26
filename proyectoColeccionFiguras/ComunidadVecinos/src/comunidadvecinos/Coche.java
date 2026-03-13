434f2daa9094ac567ad6c4044fa0df43f4bc83e5
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-03-13 18:50:55 +0100

    ok

---
+++ proyectoColeccionFiguras/ComunidadVecinos/src/comunidadvecinos/Coche.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comunidadvecinos;
+
+/**
+ *
+ * @author alumno
+ */
+public class Coche {
+
+    private String titular;
+    private String matricula;
+
+}
---
+++ proyectoColeccionFiguras/ComunidadVecinos/src/comunidadvecinos/ComunidadVecinos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comunidadvecinos;
+
+/**
+ *
+ * @author alumno
+ */
+public class ComunidadVecinos {
+
+    
+    
+    
+    
+    /**
+     * 
+     * 
+     * 
+  
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+        
+        
+    }
+    
+}
