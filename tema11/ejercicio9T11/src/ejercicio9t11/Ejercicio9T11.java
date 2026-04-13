2db7b180bfda212337f5bf09846f76cfb032d8ab
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-04-13 16:20:20 +0200

    ok

--- tema11/ejercicio9T11/src/ejercicio9t11/Ejercicio9T11.java
+++ tema11/ejercicio9T11/src/ejercicio9t11/Ejercicio9T11.java
@@ -4,6 +4,9 @@
  */
 package ejercicio9t11;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *
  * @author alumno
@@ -14,7 +17,9 @@ public class Ejercicio9T11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Map<Integer , Sorteo> sorteoLoteria = new TreeMap<>();
+         
+         
     }
     
 }
--- tema11/ejercicio9T11/src/ejercicio9t11/Sorteo.java
+++ tema11/ejercicio9T11/src/ejercicio9t11/Sorteo.java
@@ -4,10 +4,65 @@
  */
 package ejercicio9t11;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author alumno
  */
 public class Sorteo {
+    //Atributos
+    private LocalDate fecha;
+    private int[] numerosGanadores;
+    
+    //constructor
+    public Sorteo(){
+         this.fecha = LocalDate.now(); // Por defecto, la fecha de hoy
+        this.numerosGanadores = new int[4];
+       
+    }
+
+    public Sorteo(LocalDate fecha, int[] numerosGanadores) {
+        this.fecha = fecha;
+        this.numerosGanadores = numerosGanadores;
+    }
+    
+    
+    //getter y setter
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int[] getNumerosGanadores() {
+        return numerosGanadores;
+    }
+
+    public void setNumerosGanadores(int[] numerosGanadores) {
+        this.numerosGanadores = numerosGanadores;
+    }
+    
+    //metodos
+    private void generarNumAle() {
+   
+        for (int i = 0; i < numerosGanadores.length; i++) {
+            // Genera aleatorio entre 1 y 100
+             numerosGanadores[i] = (int) (Math.random() * 100) + 1;
+        }
+    }
+    
+    //to string
+
+    @Override
+    public String toString() {
+        return "Sorteo{" + "fecha=" + fecha + ", numerosGanadores=" + numerosGanadores + '}';
+    }
+    
+    
+    
     
 }
