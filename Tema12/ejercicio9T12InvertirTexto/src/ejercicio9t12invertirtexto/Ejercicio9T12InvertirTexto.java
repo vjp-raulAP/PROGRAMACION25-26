5d5da05ba39e5723b5a4b318cc4ba2459c5ffacf
vjp-raulAP <ralbalatp01@informatica.iesvalledeljerteplasencia.es>
2026-04-23 18:32:30 +0200

    ejercicio9T12 invertir frase, completado

--- Tema12/ejercicio9T12InvertirTexto/src/ejercicio9t12invertirtexto/Ejercicio9T12InvertirTexto.java
+++ Tema12/ejercicio9T12InvertirTexto/src/ejercicio9t12invertirtexto/Ejercicio9T12InvertirTexto.java
@@ -5,6 +5,7 @@
 package ejercicio9t12invertirtexto;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,44 +16,72 @@ import java.io.PrintWriter;
  * @author alumno
  */
 public class Ejercicio9T12InvertirTexto {
+    
+    //Metodo invertir un texto  pasando por parametro un texto.
+    
+    public static String invertirTexto(String texto){
+        //declaro variable donde guado el texto invertido
+         String textoInvertido= "";
+        
+        //creamos u bucle que recorrera el texto empezando por el final
+                for (int i = texto.length() - 1; i >= 0; i--) {
+                 textoInvertido =  textoInvertido + texto.charAt(i); //uso charAt para  obtener cada caracter 
+         }
+                return textoInvertido;
+    }
+    
+    //Metodo que lee un archivo  y devuelve otro archivo escrito al reves.
+    
+   public static void transformarTexto() throws FileNotFoundException,IOException{
+        String archivoEntrada = "frase.txt";  //declaro la variable en la que estara el texto
+        String archivoSalida = "fraseinvertida.txt"; //variable donde guardo textoinvertido.
+        
+        try (FileReader fr = new FileReader(archivoEntrada);
+              BufferedReader br=  new BufferedReader(fr)){
+            //1º leer el texto de archivoEntrada 
+            String  textoEntrada= br.readLine();
+            
+            //2º comprobamos que el archivo no esta a null. si no está llamamos al  metodo invertir
+            if (textoEntrada != null){
+                
+                String textoReves = invertirTexto(textoEntrada);
+                
+            //3º Abrimos flujo de escritura para guardar el texto al reves en otro archivo
+            try(FileWriter fw = new FileWriter(archivoSalida);
+                 PrintWriter pw = new PrintWriter(fw)){
+                //imprimimos el texto al reves que hemos guardado 
+                pw.print(textoReves); 
+                //damos un feedback al usuario
+                System.out.println("Archivo guardado correctamente en: " + archivoSalida);
+                
+            }
+            }else{
+                System.out.println("El archivo está vacio");
+            }
+        }
+       
+   }
+    
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     String archivoEntrada = "frase.txt";
-        String archivoSalida = "fraseinvertida.txt";
-        
-          
-    
-        
+   
 
+        //Try catch para capturar las excepciones lanzadas en los métodos 
         try {
-            // Leer el contenido del archivo
-            BufferedReader br = new BufferedReader(new FileReader(archivoEntrada));
-            String linea = br.readLine();
-            String invertida = "";
-            br.close();
-
-            if (linea == null) {
-                System.out.println("El archivo está vacío.");
-                return;
-            }
-
-            // Invertir la cadena
-                    for (int i = linea.length() - 1; i >= 0; i--) {
-             invertida += linea.charAt(i);
-         }
-
-            // Escribir el resultado en el nuevo archivo
-            FileWriter fw = new FileWriter(archivoSalida);
-            fw.write(invertida);
-            fw.close();
-
-            System.out.println("Archivo creado correctamente: " + archivoSalida);
-
+            transformarTexto();
+            //feedBack usuario
+            System.out.println("Ejecución completada");
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Error. No se encuentra el archivo 'frase.txt' ."); 
         } catch (IOException e) {
             System.out.println("Error al procesar los archivos: " + e.getMessage());
+        }catch (Exception e) {
+            System.out.println("Error inesperado: " + e.toString());
         }
     }
     }
