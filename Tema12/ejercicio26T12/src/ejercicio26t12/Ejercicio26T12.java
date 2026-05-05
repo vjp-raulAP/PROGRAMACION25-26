e441592fbe8652c65a24b2af052834c7287f182d
vjp-raulAP <114511653+vjp-raulAP@users.noreply.github.com>
2026-05-05 18:02:59 +0200

    ok

---
+++ Tema12/ejercicio26T12/src/ejercicio26t12/Ejercicio26T12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio26t12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio26T12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
